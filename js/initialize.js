d0eb7b18adb038a49a426f901f70d41d2209535f
ericjwdchen <eric.jwd.chen@gmail.com>
2016-11-12 18:27:04 -0500

    a

--- js/initialize.js
+++ js/initialize.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
+  $('.carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 });
