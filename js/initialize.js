4c0de1844d96b92679f748311b34cc0e77b7411d
ericjwdchen <eric.jwd.chen@gmail.com>
2016-11-12 19:18:36 -0500

    a

--- js/initialize.js
+++ js/initialize.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     infinite: true,
     speed: 500,
     fade: true,
-    cssEase: 'linear'
+    cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 2000
   });
