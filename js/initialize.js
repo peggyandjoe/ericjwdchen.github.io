0244b3aadac530e8f72cdacc5eb416b38d952063
ericjwdchen <eric.jwd.chen@gmail.com>
2016-12-23 22:04:24 -0600

    a

---
+++ js/initialize.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    adaptiveHeight: true,
+    infinite: true,
+    speed: 700,
+    fade: true,
+    cssEase: 'ease',
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    dots: true,
+  });
+});
