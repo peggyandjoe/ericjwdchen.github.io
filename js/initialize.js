e43c45c34831f419c6ba7fe8009389e812f6b9e9
ericjwdchen <eric.jwd.chen@gmail.com>
2016-12-29 02:21:56 -0600

    a

--- js/initialize.js
+++ js/initialize.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-  $('.carousel').slick({
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    centerMode: true,
-    centerPadding: '100px',
-    cssEase: 'ease',
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    speed: 700,
-  });
+$('.carousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  cssEase: 'ease',
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  speed: 700,
+});
 });
