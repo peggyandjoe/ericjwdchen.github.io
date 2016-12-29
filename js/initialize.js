df53932d7d7c3df9335e197934b8a4f205081fa1
ericjwdchen <eric.jwd.chen@gmail.com>
2016-12-29 02:13:09 -0600

    a

--- js/initialize.js
+++ js/initialize.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('.carousel').slick({
     adaptiveHeight: true,
     centerMode: true,
+    centerPadding: '50px',
     infinite: true,
     speed: 700,
     cssEase: 'ease',
