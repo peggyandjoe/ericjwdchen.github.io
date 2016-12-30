eead7a324739bcee2c0a769c17ba4e5b766de2bf
ericjwdchen <eric.jwd.chen@gmail.com>
2016-12-30 10:02:18 -0600

    a

--- js/initialize.js
+++ js/initialize.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     slidesToShow: 1,
     arrows: true,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     cssEase: 'ease',
     dots: true,
     infinite: true,
